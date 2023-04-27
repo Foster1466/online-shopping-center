d4cd9d59086925da84448c9388328709b7e9fe91
Jasmeet <jasmeetsingh4263@gmail.com>
2023-04-27 14:30:57 -0700

    -- Added admin screen -- Added Admin user delete functionality -- Added Edit screen and get updated user information feature I'm on a roll

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,14 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {productListReducer, productDetailsReducer} from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
-import {userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from './reducers/userReducers'
+import {userLoginReducer,   
+        userRegisterReducer, 
+        userDetailsReducer, 
+        userUpdateProfileReducer, 
+        userListReducer, 
+        userDeleteReducer,
+        userUpdateReducer,
+    } from './reducers/userReducers'
 import {orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer} from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -15,10 +22,15 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    userList: userListReducer,
+    userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
+    
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
     orderListMy: orderListMyReducer,
+
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ?
